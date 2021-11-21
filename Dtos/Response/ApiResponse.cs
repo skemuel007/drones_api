383429bd5db5048a3176ce7a1112ed84c2858435
extremecoder08 <salvation.stanley-kemuel@intelligra.io>
2021-11-21 15:42:12 +0100

    Global error handler, Api Health check controller created

---
+++ Dtos/Response/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace drones_api.Dtos.Response
+{
+    public class ApiResponse<T>
+    {
+
+        public ApiResponse()
+        {
+
+        }
+        public ApiResponse(T data, string message, bool status)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+
+        }
+        public bool Status { get; set; } = true;
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ Dtos/Response/HealthReportResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace drones_api.Dtos.Response
+{
+    public class HealthReportResponseDto
+    {
+        public Boolean Status { get; }
+        public TimeSpan TotalDuration { get; }
+    }
+}
