ec5c75a893cc7935eab05cf9ef6424cc12e01c88
extremecoder08 <salvation.stanley-kemuel@intelligra.io>
2021-11-24 15:55:04 +0100

    Removing secret

--- Services/Implementations/DroneRepository.cs
+++ Services/Implementations/DroneRepository.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using drones_api.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace drones_api.Services.Implementations
 {
-    public class DroneRepository
+    public class DroneRepository : RepositoryBase<Drone>, IDroneRepository
     {
+        public DroneRepository(DronesApiContext context) : base(context) { }
     }
 }
